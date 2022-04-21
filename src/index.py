a45097355beef4b00f8f1e3858b71e1a8f5da532
satyakisikdar <ssikdar@nd.edu>
2022-04-20 23:57:38 -0400

    major commit. standardized Index writing. References and Works are working. Need to add authorship and concept info for Works.

--- src/index.py
+++ src/index.py
@@ -9,12 +9,12 @@ Concept Works: concept_id,  num_works [work1, score1, work2, score2, ... ]
 """
 import abc
 import os
-import struct
 
 import pandas as pd
 
+from src.encoder import EncoderDecoder
 from src.objects import Work
-from src.utils import Paths, Indices
+from src.utils import Paths, IDMap, reconstruct_abstract, strip_accents
 
 
 class BaseIndexer:
@@ -22,7 +22,7 @@ class BaseIndexer:
     Base class for indexers
     """
 
-    def __init__(self, paths: Paths, indices: Indices, kind: str):
+    def __init__(self, paths: Paths, indices, kind: str):
         self.paths = paths
         self.indices = indices
         self.kind = kind
@@ -32,8 +32,10 @@ class BaseIndexer:
 
         self.offset_path = self.index_path / 'offsets.txt'
         self.data_path = self.index_path / 'data.txt'
-
         self.offsets = self.read_offsets()
+
+        self.encoder = EncoderDecoder()
+        self.decoder = EncoderDecoder()  # just me being lazy
         return
 
     def read_offsets(self) -> pd.DataFrame:
@@ -47,8 +49,29 @@ class BaseIndexer:
 
         return pd.read_csv(self.offset_path, index_col=0, engine='c').to_dict('index')
 
+    def write_index(self, bites: bytes, id_: int):
+        """
+        Update the offsets dictionary
+        Write the id, offset, and len(bites) into offsets file
+        Write the bites to the data file
+        """
+        if len(self.offsets) == 0:
+            previous_offset = 0
+            previous_len = 0
+        else:
+            last_key = list(self.offsets.keys())[-1]
+            previous_offset, previous_len = self.offsets[last_key]['offset'], self.offsets[last_key]['len']
+
+        offset = previous_offset + previous_len
+        self.offsets[id_] = {'offset': offset, 'len': len(bites)}
+
+        with open(self.offset_path, 'a') as offset_writer, open(self.data_path, 'ab') as data_writer:
+            offset_writer.write(f'{id_},{offset},{len(bites)}\n')  # write the offset
+            data_writer.write(bites)  # write the bytes
+        return
+
     @abc.abstractmethod
-    def get_bytes(self, entity) -> bytes:
+    def convert_to_bytes(self, entity) -> bytes:
         pass
 
     @abc.abstractmethod
@@ -56,7 +79,7 @@ class BaseIndexer:
         pass
 
     @abc.abstractmethod
-    def parse_entry(self, offset: int):
+    def parse_bytes(self, offset: int, reader=None):
         pass
 
 
@@ -66,33 +89,32 @@ class RefIndexer(BaseIndexer):
     format: #work_id #refs ref1 ref2 .. #citations cite1 cite2
     """
 
-    def __init__(self, paths: Paths, indices: Indices):
+    def __init__(self, paths: Paths, indices):
         super().__init__(paths, indices, kind='references')
         return
 
-    def get_bytes(self, work: Work) -> bytes:
+    def convert_to_bytes(self, work: Work) -> bytes:
         """
         Return bytes for the work
         #work_id, number of references, w1, w2, ...., wn
         """
-        num_refs = len(work.references)
-
         bites = [
-            struct.pack('c', '#'.encode('utf-8')),  # add a # sign
-            struct.pack('Q', work.work_id),  # work id comes firs
-            struct.pack('L', num_refs),  # number of references
+            self.encoder.encode_id(id_=work.work_id),
+            self.encoder.encode_long_int(li=len(work.references))
         ]
 
+        # add references
         bites.extend(
-            struct.pack('Q', ref_w) for ref_w in work.references
+            self.encoder.encode_long_long_int(lli=ref_w) for ref_w in work.references
         )
 
         # add citations
-        num_cites = len(work.citing_works)
-        bites.append(struct.pack('L', num_cites))
+        bites.append(
+            self.encoder.encode_long_int(li=len(work.citing_works))
+        )
 
         bites.extend(
-            struct.pack('Q', cite_w) for cite_w in work.citing_works
+            self.encoder.encode_long_long_int(lli=cite_w) for cite_w in work.citing_works
         )
         return b''.join(bites)
 
@@ -112,47 +134,119 @@ class RefIndexer(BaseIndexer):
         work.populate_references(self.indices)
         work.populate_citations(self.indices)
 
-        bites = self.get_bytes(work=work)
-        # print(f'{work_id=} has {len(work.references)} references {work.citations} citations {len(bites)} bytes')
-
-        if len(self.offsets) == 0:
-            previous_offset = 0
-            previous_len = 0
-        else:
-            last_key = list(self.offsets.keys())[-1]
-            previous_offset, previous_len = self.offsets[last_key]['offset'], self.offsets[last_key]['len']
-
-        offset = previous_offset + previous_len
-        self.offsets[work_id] = {'offset': offset, 'len': len(bites)}
-
-        with open(self.offset_path, 'a') as offset_writer, open(self.data_path, 'ab') as data_writer:
-            offset_writer.write(f'{work_id},{offset},{len(bites)}\n')  # write the offset
-            data_writer.write(bites)  # write the bytes
-
+        bites = self.convert_to_bytes(work=work)
+        self.write_index(bites=bites, id_=work_id)
         return
 
-    def parse_entry(self, offset: int) -> Work:
-        with open(self.data_path, 'rb') as reader:
-            reader.seek(offset)
-            h_, = struct.unpack('c', reader.read(1))
-            assert h_.decode('utf-8') == '#', 'missing #, something wrong with the index'
+    def parse_bytes(self, offset: int, reader=None) -> Work:
+        if reader is None:
+            reader = open(self.data_path, 'rb')
 
-            id_, = struct.unpack('Q', reader.read(8))
+        reader.seek(offset)
 
-            print(f'reading work_id: {id_}')
-            num_refs, = struct.unpack('L', reader.read(8))
-            print(f'References: {num_refs}')
-            refs = set()
-            for _ in range(num_refs):
-                ref_, = struct.unpack('Q', reader.read(8))
-                refs.add(ref_)
+        work_id = self.decoder.decode_id(reader)
+        print(f'{work_id=}')
 
-            num_cites, = struct.unpack('L', reader.read(8))
-            print(f'Citations: {num_cites}')
-            cites = set()
-            for _ in range(num_cites):
-                cite_, = struct.unpack('Q', reader.read(8))
-                cites.add(cite_)
+        num_refs = self.decoder.decode_long_int(reader)
+        print(f'{num_refs=}')
 
-        work = Work(work_id=id_, references=refs, citing_works=cites, citations=num_cites)
+        refs = {self.decoder.decode_long_long_int(reader) for _ in range(num_refs)}
+
+        num_cites = self.decoder.decode_long_int(reader)
+        print(f'{num_cites=}')
+
+        cites = {self.decoder.decode_long_long_int(reader) for _ in range(num_cites)}
+
+        return Work(work_id=work_id, citing_works=cites, references=refs)
+
+
+class WorkIndexer(BaseIndexer):
+    """
+    Write work information into a binary file
+    #,work_id, type, DOI, title, venue_id, date, year, abstracts
+    """
+
+    def __init__(self, paths: Paths, indices, id_map: IDMap):
+        super().__init__(paths, indices, kind='works')
+        self.id_map = id_map
+        return
+
+    def process_entry(self, work_id: int):
+        if work_id in self.offsets:  # already computed
+            return
+
+        work = Work(work_id=work_id, paths=self.paths)
+        work.populate_info(indices=self.indices)
+        work.populate_venue(indices=self.indices, id_map=self.id_map)
+        bites = self.convert_to_bytes(work=work)
+
+        self.write_index(id_=work_id, bites=bites)
+        return
+
+    def convert_to_bytes(self, work) -> bytes:
+        bites = [
+            self.encoder.encode_id(id_=work.work_id),
+            self.encoder.encode_int(i=work.part_no),
+            self.encoder.encode_work_type(typ=work.type)
+        ]
+
+        cleaned_title = strip_accents(work.title)
+        if not cleaned_title.isascii():  # dont write title if it's not ascii after cleaning
+            cleaned_title = ''
+
+        bites.extend([
+            self.encoder.encode_string(string=work.doi),  # DOI
+            self.encoder.encode_string(string=cleaned_title),  # title
+
+            self.encoder.encode_int(i=work.publication_year),
+            self.encoder.encode_string(string=work.publication_date),
+
+            self.encoder.encode_venue(venue=work.venue),  # venue
+        ])
+
+        abstract = reconstruct_abstract(work.abstract_inverted_index)  # abstract
+        if not abstract.isascii():  # dont write non-ascii abstracts
+            abstract = ''
+
+        bites.append(self.encoder.encode_string(abstract))
+        # TODO: consider adding number of references and citations to the byte array
+
+        return b''.join(bites)
+
+    def parse_bytes(self, offset: int, reader=None) -> Work:
+        if reader is None:
+            reader = open(self.data_path, 'rb')
+        reader.seek(offset)
+
+        work_id = self.decoder.decode_id(reader)
+        print(f'{work_id=}')
+
+        part_no = self.decoder.decode_int(reader)
+        print(f'{part_no=}')
+
+        work_type = self.decoder.decode_work_type(reader)
+        print(f'{work_type=}')
+
+        doi = self.decoder.decode_string(reader)
+        print(f'{doi=}')
+
+        title = self.decoder.decode_string(reader)
+        print(f'{title=}')
+
+        year = self.decoder.decode_int(reader)
+        print(f'{year=}')
+
+        assert year < 3000, f'year {year} out of range!'
+
+        date = self.decoder.decode_string(reader)
+        print(f'{date=}')
+
+        venue = self.decoder.decode_venue(reader)
+
+        abstract = self.decoder.decode_string(reader)
+        reader.close()
+
+        work = Work(work_id=work_id, part_no=part_no, type=work_type, doi=doi, title=title, publication_year=year,
+                    publication_date=date, venue=venue, abstract=abstract)
+
         return work
