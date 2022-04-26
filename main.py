1aedc7b3f169c04754da3b835f1fc026c93737da
ssikdar <ssikdar@iu.edu>
2022-04-26 17:52:39 -0400

    new runner for parallel work id indexer

--- main.py
+++ main.py
@@ -8,6 +8,21 @@ from tqdm.auto import tqdm
 from src.entities import Works
 from src.utils import Paths, Indices, IDMap
 from src.index import WorkIndexer, ConceptIndexer, RefIndexer
+from joblib import Parallel, delayed, parallel_backend
+
+
+## parallelize
+def parallel_write(work_id, work_indexer):
+    if work_id in work_indexer:
+        return
+
+    try:
+        work = work_indexer[work_id]
+        bites = work_indexer.convert_to_bytes(work)
+        work_indexer.dump_bytes(work_id=work_id, bites=bites)
+    except Exception as e:
+        print(f'Exception {e=} for {work_id=}')
+    return
 
 
 def parse(num_workers):
@@ -28,34 +43,39 @@ def index_refs(num_workers):
     id_map = IDMap(paths=paths)
     concept_indexer = ConceptIndexer(paths=paths, indices=indices)
     work_indexer = WorkIndexer(paths=paths, indices=indices, id_map=id_map)
-    ref_indexer = RefIndexer(paths=paths, indices=indices)
+    # ref_indexer = RefIndexer(paths=paths, indices=indices)
 
     complex_network = 34947359
     concept = concept_indexer.parse_bytes(offset=concept_indexer.offsets[complex_network]['offset'])
     print(f'{concept=}')
+
     work_ids = [w for w, _ in concept.tagged_works]
     print(f'{len(work_ids)=:,}')
 
-    # work_ids = indices['works'].index
+    work_indexer.update_index_from_dump()  # update the data and offset files
+    with parallel_backend('threading', n_jobs=num_workers):
+        Parallel()(
+            delayed(parallel_write)(work_id, work_indexer) for work_id in tqdm(work_ids)
+        )
+    work_indexer.update_index_from_dump()  # update the data and offset files
 
-    for work_id in tqdm(work_ids):
-        try:
-            w = Work(work_id=work_id, paths=paths)
-            w.load(ref_indexer=ref_indexer, work_indexer=work_indexer)
-
-        except Exception as e:
-            print(f'{work_id=} {e=}')
     return
-    #
-    # with parallel_backend('threading', n_jobs=num_workers):
-    #     Parallel()(
-    #         delayed(ref_index.process_entry)(work_id) for work_id in tqdm(work_ids)
-    #     )
+
+
+#     for work_id in tqdm(work_ids):
+#         try:
+#             w = Work(work_id=work_id, paths=paths)
+#             w.load(ref_indexer=ref_indexer, work_indexer=work_indexer)
+
+#         except Exception as e:
+#             print(f'{work_id=} {e=}')
+#     return
+#
 
 
 def main():
     # parse(num_workers=6)
-    index_refs(num_workers=10)
+    index_refs(num_workers=6)
     return
 
 
