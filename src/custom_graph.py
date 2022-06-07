5571062031708b11df0917e95ba8d3786e42d464
satyakisikdar <ssikdar@nd.edu>
2022-06-07 17:34:04 -0400

    updated descriptions, str, and neighbors  methods

--- src/custom_graph.py
+++ src/custom_graph.py
@@ -1,14 +1,16 @@
 import igraph as ig
 
 DESCRIPTIONS = {
-    'cite_ref': '''Directed graph linking works with references (out) and citations (in).
-    work -> reference''',
-    'related': '''Directed graph linking a work with related works.
-    work -> related_work''',
-    'concepts': '''Weighted directed bipartite graph linking works with concepts.
-    work -wt-> concept''',
-    'authorships': '''Directed graph linking a work with authors and institutions.
-    Links: work -> author, work -> institute, author -> institute'''
+    'cite_ref':
+        'Directed graph linking works with references (out) and citations (in)',
+    'related':
+        'Directed graph linking a work with related works.',
+    'concept':
+        'Weighted bipartite graph linking works with concepts.',
+    'authorship':
+        'Directed graph linking a work with authors and institutions.\nwt = 1: first, 2: middle, 3: last',
+    'venue':
+        'Graph linking work with a venue.'
 }
 
 
@@ -20,18 +22,18 @@ class CustomGraph(ig.Graph):
 
     def __init__(self, kind: str, *args, **kwds):
         super().__init__(*args, **kwds)
-        self.kind = 'kind'
+        self.kind = kind
         self.description = DESCRIPTIONS[kind]
         return
 
-    def has_node(self, name: str) -> bool:
+    def has_vertex(self, name: str) -> bool:
         """
-        returns the node object if node with the name exists, else return False
+        returns True if node with the name exists, else return False
         """
         try:
-            v = self.vs.find(name=name)
-            present = v
-        except ValueError:
+            self.vs.find(name)
+            present = True
+        except (ValueError, KeyError):
             present = False
         return present
 
@@ -39,12 +41,18 @@ class CustomGraph(ig.Graph):
         """
         Create a new node if needed, else return the existing node
         """
-        val = self.has_node(name=name)
-        if isinstance(val, bool) and not val:
-            return self.add_vertex(name=name, **kwds)
+        if self.has_vertex(name=name):
+            # print(f'Existing vertex {name!r} found')
+            return
         else:
-            return val
+            return super().add_vertex(name=name, **kwds)
 
     def __str__(self) -> str:
-        st = f'{self.kind!r} |V| = {self.vcount():,}\t |E| = {self.ecount():,}\n{self.description}'
+        st = f'Name: {self.kind!r} {self.summary()}\nDescription: {self.description!r}'
         return st
+
+    def neighbors(self, *args, **kwargs):
+        """
+        Return the names of vertices that are neighbors
+        """
+        return [self.vs[nbr]['name'] for nbr in super().neighbors(*args, **kwargs)]
