709cffa73914ed1f12a520447cb2b559389624ee
satyakisikdar <ssikdar@nd.edu>
2023-09-20 15:04:25 -0400

    more refactoring

--- preprocessing/filter_dataset.py
+++ preprocessing/filter_dataset.py
@@ -306,7 +306,7 @@ def write_other_filtered_tables_v2(whole_parq_path, filt_parq_path, work_ids):
                         [filt_df.referenced_work_id.isin(work_ids)]
                     )
 
-                filt_df.to_parquet(parq_filename, engine='fastparquet')
+                filt_df.to_parquet(parq_filename, engine='pyarrow')
                 row_counts += len(filt_df)
 
                 pbar.update(1)
